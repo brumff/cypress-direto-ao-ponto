0bbd0af3bdf68f8d941dd78a2c333ac756c00465
brumff <brunameiraf@gmail.com>
2023-03-06 16:27:36 -0300

    Curso cypress

---
+++ cypress/e2e/finances.cy.js
@@ -0,0 +1,49 @@
+describe('Transações', () => {
+
+    beforeEach(() => {
+        cy.visit("https://devfinance-agilizei.netlify.app/")
+    });
+    it('Cadastrar uma entrada', () => {
+        
+
+        criarTransacao('Freela', 250)
+
+        cy.get("tbody tr td.description").should("have.text", "Freela")
+    });
+
+    it('Cadatrar uma saída', () => {
+       
+        criarTransacao('Cinema', -40)
+
+        cy.get('tbody tr td.description').should('have.text', 'Cinema')
+    });
+
+    it('Exluir transação', () => {
+       
+        criarTransacao('Freela', 100)
+        criarTransacao('Mesada', 100)
+
+        // cy.contains('.description', 'Freela')
+        //  .parent()
+        //  .find('img')
+        //  .click()
+
+        cy.contains('.description', 'Freela')
+            .siblings()
+            .children('img')
+            .click()
+
+        cy.get('tbody tr').should('have.length', 1)
+
+    });
+
+});
+
+function criarTransacao(descricao, valor) {
+        cy.contains("Nova Transação").click()
+        cy.get('#description').type(descricao)
+        cy.get('#amount').type(valor)
+        cy.get('#date').type("2023-02-15")
+
+        cy.contains('button', 'Salvar').click()
+}
